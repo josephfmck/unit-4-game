2114fe8c02120077d9262148fec949b40f45f15b
Joe <josephfmck@gmail.com>
2018-09-05 11:13:51 -0500

    finished game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # unit-4-game
+This is a Crystal Collector game where you have to click 4 crystals that each have different values and add them up to the random score provided.
+Once you win or lose the game, the game resets and randomly chooses a new number for the score provided and for each of the crystals.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,122 @@
-var targetNumber = 53;
+//sets up random number user is trying to match
+$(document).ready(function () 
+{
+    var random = Math.floor(Math.random() * 121 + 19)  //random number from 19 to 120
+    $('#number-to-guess').text(random);     //generates random into text number-to-guess ID
+    
 
-$("#number-to-guess").text(targetNumber);
 
-//var of counts of your score
-var counter = 0;
-var wins = 0;
-var losses = 0;
-$("#numberWins").text(wins);
-$("#numberLosses").text(losses);
-$("#totalScore").text(counter);
+    //	setting up four random numbers array, total variable and printing the wins & losses variables	
+    var generatedNumbers = []       //array for randomly generated numbers
+    var total = 0;
+    var wins = 0;
+    var losses = 0;
+    $('#numberWins').text(wins);
+    $('#numberLosses').text(losses);
 
-var numberOptions = [10, 5, 3, 7];
+    //sets up random numbers for each crystal
+    function fourRandomNumbers() 
+    {
+        for (var i = 0; i < 4; i++)    
+        {
+            var num = Math.floor(Math.random() * 13 + 1); //random number from 1 to 12
+            generatedNumbers.push(num);         // adds num to generatedNumbers array
+        }
+    }
+    fourRandomNumbers();
 
-  // Next we create a for loop to create crystals for every numberOption.
-  for (var i = 0; i < numberOptions.length; i++) {
-
-    // For each iteration, we will create an imageCrystal
-    var imageCrystal = $("<img>");
-
-    // First each crystal will be given the class ".crystal-image".
-    // This will allow the CSS to take effect.
-    imageCrystal.addClass("crystal-image");
-
-    // Each imageCrystal will be given a src link to the crystal image
-    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    $("#crystals").append(imageCrystal);
-  }
-
-  // This time, our click event applies to every single crystal on the page. Not just one.
-  //$("")  is a jQuery selector    ".crystal-image" makes it an object      .on is a method that uses it with the function.
-  //("click", function()) is a callback function and is executec by the click
-  $(".crystal-image").on("click", function() {
-
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
-    counter += crystalValue;
-
-    // All of the same game win-lose logic applies. So the rest remains unchanged.
-    alert("New score: " + counter);
-        $("#totalScore").text(counter);
-
-    if (counter === targetNumber) {
-      alert("You win!");
-      wins++;
-      $("#numberWins").text(wins);
+    //resets the game
+    function reset() 
+    {
+        random = Math.floor(Math.random()* 121 + 19); //starts with random number from 19 to 120
+        $('#number-to-guess').text(random); 
+        generatedNumbers = [];
+        fourRandomNumbers();
+        total = 0;
+        $('#totalScore').text(total); //shows your total score in text in totalScore ID
     }
 
-    else if (counter >= targetNumber) {
-      alert("You lose!!");
-      losses++;
-      $("#numberLosses").text(losses);
+    //adds the wins to the total
+    function winner() 
+    {
+        alert("You won!!!");
+        wins++;
+        $('#numberWins').text(wins);
+        reset();            //executes reset function after winning
     }
 
-  });
+    //adds the losses to the total
+    function loser() 
+    {
+        alert("You lose!!");
+        losses++;
+        $('#numberLosses').text(losses);
+        reset();
+    }
 
+    //sets up click for first crystal
+    $('#one').on('click', function () 
+    {
+        total = total + generatedNumbers[0];        //adds the first index of generatedNumbers to the total score
+        $('#totalScore').text(total);
 
+        //sets win/lose conditions
+        if (total == random) 
+        {
+            winner();
+        }
+        else if (total > random) 
+        {
+            loser();
+        }
+    })
+
+    $('#two').on('click', function () 
+    {
+        total = total + generatedNumbers[1];
+        $('#totalScore').text(total);
+        
+        //sets win/lose conditions
+        if (total == random) 
+        {
+            winner();
+        }
+        else if (total > random) 
+        {
+            loser();
+        }
+    })
+
+    $('#three').on('click', function () 
+    {
+        total = total + generatedNumbers[2];
+        $('#totalScore').text(total);
+
+        //sets win/lose conditions
+        if (total == random) 
+        {
+            winner();
+        }
+        else if (total > random) 
+        {
+            loser();
+        }
+    })
+
+    $('#four').on('click', function () 
+    {
+        total = total + generatedNumbers[3];
+        $('#totalScore').text(total);
+
+        //sets win/lose conditions
+        if (total == random) 
+        {
+            winner();
+        }
+        else if (total > random) 
+        {
+            loser();
+        }
+    });
+
+});
